710b219ee5ce0479a43285ae9563742e12fdb792
Jamaouiomar <92652548+Jamaouiomar@users.noreply.github.com>
2023-12-09 23:42:22 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,6 @@
+//import app from backend app
+const app = require("./backend/app");
+//httplocalhost 3000
+app.listen(3000,()=>{
+    console.log("Express Server Is Listening On PORT 3000...")
+});
